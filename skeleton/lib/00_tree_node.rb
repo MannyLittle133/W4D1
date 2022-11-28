563c84f78dec64ef6293577ac0b0bc9a295b3959
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-11-28 14:09:37 -0500

    completed initialize started parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
+    attr_accessor :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        if !node.children.include?(self)
+            node.children << self 
+        else
+            node.children = 
+        end
+    end
+
+    def add_child(node)
+
+    end
+
 
 end
